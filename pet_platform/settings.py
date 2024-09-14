3a3ee166ce33f426e6598ef0a23e3a72f6f3b25b
Karen <karen2006balayan@gmail.com>
2024-09-14 12:23:23 +0400

    second commit

--- pet_platform/settings.py
+++ pet_platform/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "marketplace",
+    "marketplace.apps.MarketplaceConfig",
 ]
 
 MIDDLEWARE = [
@@ -120,10 +120,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "marketplace/", "static/")
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
 
 
 
